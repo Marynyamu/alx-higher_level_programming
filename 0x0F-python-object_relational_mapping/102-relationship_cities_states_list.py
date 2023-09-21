99f91963c6b6526fe49e8ae3c75984f3f05fa86f
wanjanyamu01@gmail.com <wanjanyamu01@gmail.com>
2023-09-21 06:33:17 -0700

    orm

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_101_usa.
-# Usage: ./102-relationship_cities_states_list.py <mysql username> /
-#                                                 <mysql password> /
-#                                                 <database name>
+"""
+Lists all City objects from the database hbtn_0e_101_usa.
+Usage: ./102-relationship_cities_states_list.py <mysql username> /
+                                                <mysql password> /
+                                                <database name>
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,7 +12,7 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Python file that contains the class definition of a State and an instance
+Defines a state model that contain the class definition
+ of a State and an instance Base = declarative_base()
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,12 +12,16 @@ Base = declarative_base()
 
 class State(Base):
     """
-    State class that inherits from Base
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column
+     of an auto-generated, unique integer, can't
+      be null and is a primary key
+    class attribute name that represents a column
+     of a string with maximum 128 characters and
+      can't be null
 
-    Attributes:
-        id: Id state
-        name: Name of state
     """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
